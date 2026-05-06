cb5fc3b1737a0d5cf0739b5744ccf91261c5a9f1
garrido02 <sergio_garrido_18@hotmail.com>
2026-05-06 07:45:14 -0400

    stacksmash fix

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # 2. Dar permissão de execução ao script principal
-if [ -f "stacksmash.py" ]; then
+if [ -f "stacksmash" ]; then
     chmod +x stacksmash
     # 3. Mover para /usr/local/bin
     cp stacksmash /usr/local/bin/
